7b9c184def82fed40177a1a33d95b8907c4f995e
Diego Augusto <diih_augusto@live.com>
2019-04-26 00:45:38 -0300

    Ajustes de layout

--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class ProductDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+    static navigationOptions = {
+        title: 'Detalhes do Produto',
+        headerStyle: {
+          backgroundColor: '#01a699',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      };
 
     render() {
         const { navigation } = this.props;
